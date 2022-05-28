0c2b68c6eba6dc168699a2974846e12df9f05759
mayukoooo <yamagishi.work3@gmail.com>
2022-05-28 17:40:19 +0900

    Activitiesのリスト部分修正

--- front/components/Activities.vue
+++ front/components/Activities.vue
@@ -7,12 +7,13 @@
       <img src="~/assets/img/mokumoku.png" height="200px">
       <div class="text">
         <h3>活動内容</h3>
-        <p>・学習の習慣化と女性の「エンジニアになる」という選択肢を当たり前にすることを目的に開催。<br>
-          ・毎週日曜日20:00〜21:30にGoogle Meetにて実施。<br>
-          ・最初の30分で1週間の振り返りと情報共有、残りの1時間でもくもく集中して学習に取り組む。<br>
-          ・毎日学習したことをSlackにて報告し合う。<br>
-          <font color="#FF1616"><b>・培った力：チームワーク、企画力、発信力、バックグラウンドの異なる方々を巻き込む力</b></font><br>
-        </p>
+        <ul>
+          <li>学習の習慣化と女性の「エンジニアになる」という選択肢を当たり前にすることを目的に開催。</li>
+          <li>毎週日曜日20:00〜21:30にGoogle Meetにて実施。</li>
+          <li>最初の30分で1週間の振り返りと情報共有、残りの1時間でもくもく集中して学習に取り組む。</li>
+          <li>毎日学習したことをSlackにて報告し合う。</li>
+          <li><font color="#FF1616"><b>培った力：チームワーク、企画力、発信力、バックグラウンドの異なる方々を巻き込む力</b></font></li>
+        </ul>
       </div>
     </div>
   </div>
@@ -23,11 +24,12 @@
       <img src="~/assets/img/nippou.png" height="200px">
       <div class="text">
         <h3>活動内容</h3>
-        <p>・1日のTODOを明確化し、その日の成果をアウトプットとして残すことで継続化を図る。<br>
-          ・2022年2月より毎日欠かさず作成。<br>
-          <font color="#FF1616"><b>・培った力：学習の習慣、学習したことの言語化力、継続力</b></font><br>
-          ・日報URL：<br>
-        </p>
+        <ul>
+          <li>1日のTODOを明確化し、その日の成果をアウトプットとして残すことで継続化を図る。</li>
+          <li>2022年2月より毎日欠かさず作成。</li>
+          <li><font color="#FF1616"><b>培った力：学習の習慣、学習したことの言語化力、継続力</b></font></li>
+          <li>日報URL：※工事中</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -38,9 +40,16 @@
       <a href="https://qiita.com/Mayuko_Yamagishi" target="_blank" rel="noopener noreferrer"><img src="~/assets/img/qiita.png" height="200px"></a>
       <div class="text">
         <h3>活動内容</h3>
-        <p>・誰かに教えるつもりで書くことで、「何故か」の部分の理解を深める。</p>
-        <p>・2022/4/23<a href="https://qiita.com/Mayuko_Yamagishi/items/b18646cbff7f95d37cc1" target="_blank" rel="noopener noreferrer">【ポートフォリオ】完全未経験が２ヶ月半で「自己内省アプリ」を完全SPA化して作ってみました【Nuxt, Rails, AWS</a></p>
-        <p>・2022/5/1<a href="https://qiita.com/Mayuko_Yamagishi/items/046457044e5409cde4c2" target="_blank" rel="noopener noreferrer"> SPAでリロードした時にaccess deniedになってしまう時の対処法</a></p>
+        <ul class="bottom">
+          <li>誰かに教えるつもりで書くことで、「何故か」の部分の理解を深める。</li>
+          <li><font color="#FF1616"><b>培った力：アウトプット力、根本的に理解しようとする姿勢</b></font></li>
+        </ul>
+
+        <h3>投稿記事</h3>
+        <ul>
+          <li>2022/4/23<a href="https://qiita.com/Mayuko_Yamagishi/items/b18646cbff7f95d37cc1" target="_blank" rel="noopener noreferrer">【ポートフォリオ】完全未経験が２ヶ月半で「自己内省アプリ」を完全SPA化して作ってみました【Nuxt, Rails, AWS</a></li>
+          <li>2022/5/1<a href="https://qiita.com/Mayuko_Yamagishi/items/046457044e5409cde4c2" target="_blank" rel="noopener noreferrer"> SPAでリロードした時にaccess deniedになってしまう時の対処法</a></li>
+        </ul>
       </div>
     </div>
   </div>
@@ -51,11 +60,12 @@
       <a href="https://utopian-moat-987.notion.site/AtCoder-261f50d6130c476e911eb2feed5438c2" target="_blank" rel="noopener noreferrer"><img src="~/assets/img/atcoder.png" height="200px"></a>
       <div class="text">
         <h3>活動内容</h3>
-        <p>・基礎力強化のためにAtCoder Beginner ContestのA,B問題（過去問）に取り組む。<br>
-           ・週1回競プロ勉強会に参加し、モチベーションを高める。メンバーの解法を見ることで新しいアプローチ方法を学ぶ。<br>
-           <font color="#FF1616"><b>・培った力：コーディング基礎力</b></font><br>
-           ・解答URL：<a href="https://utopian-moat-987.notion.site/AtCoder-261f50d6130c476e911eb2feed5438c2" target="_blank" rel="noopener noreferrer">https://utopian-moat-987.notion.site/AtCoder-261f50d6130c476e911eb2feed5438c2</a>
-        </p>
+        <ul>
+          <li>基礎力強化のためにtCoder Beginner ContestのA,B問題（過去問）に取り組む。</li>
+          <li>週1回競プロ勉強会に参加し、モチベーションを高める。メンバーの解法を見ることで新しいアプローチ方法を学ぶ。</li>
+          <li><font color="#FF1616"><b>培った力：コーディング基礎力</b></font></li>
+          <li>解答URL：<a href="https://utopian-moat-987.notion.site/AtCoder-261f50d6130c476e911eb2feed5438c2" target="_blank" rel="noopener noreferrer">https://utopian-moat-987.notion.site/AtCoder-261f50d6130c476e911eb2feed5438c2</a></li>
+        </ul>
       </div>
     </div>
   </div>
@@ -77,5 +87,11 @@ export default {
 }
 .activity {
   margin-bottom: 80px;
+}
+li {
+  font-size: 14.4px;
+}
+.bottom {
+  margin-bottom: 16px;
 }
 </style>
